749a15691f5e08cbdac029be7248ca0c0885c805
vituyac <skisterev78@mail.ru>
2024-06-01 21:27:42 +0300

    edit model connect

--- backend/directions/views.py
+++ backend/directions/views.py
@@ -30,27 +30,42 @@ class DirectionPriceAPIView(APIView):
         StdNorm = 22683.868561457934
         MeanNorm = 20875.7709068807
         
-        date = request.data['date']
-        dep_time = request.data['dep_time']
-        direction1 = Direction.objects.get(id=request.data['from'])
-        direction2 = Direction.objects.get(id=request.data['to'])
-        fromm = direction1.city
-        to = direction2.city
-        clas = request.data['clas']
-        time_taken = request.data['time_taken']
-        stop = request.data['stop']
-
+        directionfrom = Direction.objects.get(id=request.data['from'])
+        directionto = Direction.objects.get(id=request.data['to'])
+        fr = directionfrom.city
+        to = directionto.city
+        if (fr == "Мумбаи"):
+            fr = "Mumbai"
+        if (to == "Мумбаи"):
+            to = "Mumbai"
+        if (fr == "Дели"):
+            fr = "Delhi"
+        if (to == "Дели"):
+            to = "Delhi"
+        if (fr == "Калькутта"):
+            fr = "Kolkata"
+        if (to == "Калькутта"):
+            to = "Kolkata"
+        if (fr == "Хайдарабад"):
+            fr = "Hyderabad"
+        if (to == "Хайдарабад"):
+            to = "Hyderabad"
+        if (fr == "Ченнаи"):
+            fr = "Chennai"
+        if (to == "Ченнаи"):
+            to = "Chennai"
+        if (fr == "Бангалор"):
+            fr = "Bangalore"
+        if (to == "Бангалор"):
+            to = "Bangalore"
         # date / dep_time / from / to / clas / time_taken / stop -> str
-        # Вить, это твой запрос
         #response = ["12-03", "09:00", "Mumbai", "Delhi", "economy", "10h 50m", "0"]
-        response = [date, dep_time, fromm, to, clas, time_taken, stop]
-        # result = price -> int
-
+        response = [request.data['date'], request.data['dep_time'], fr, to, request.data['clas'], request.data['time_taken'], request.data['stop']]
+    
         From = ['Mumbai', 'Delhi', 'Hyderabad', 'Chennai', 'Bangalore', 'Kolkata']
         To = ['Mumbai', 'Delhi', 'Hyderabad', 'Chennai', 'Bangalore', 'Kolkata']
         Mean = [283.919857, 781.114409, 2.330489, 2.385270, 0.311351, 732.907093, 0.923924, 3.942571]
         Std = [45.747274, 321.879003, 1.767264, 1.769400, 0.463047, 431.536740, 0.397781, 1.929122]
-
         response[0] = int(response[0][3:5] + response[0][:2])
         response[1] = int(response[1][:2]) * 60 + int(response[1][3:])
         response[2] = From.index(response[2])
@@ -61,7 +76,6 @@ class DirectionPriceAPIView(APIView):
         flagOfClas = 1 if response[4] else 0
         for i in range(7):
             response[i] = (response[i] - Mean[i]) / Std[i]
-        print(20 * '\n', response)
         airline = ['SpiceJet', 'GO FIRST', 'Indigo', 'Air India', 'AirAsia', 'Trujet', 'Vistara', 'StarAir']
         result = []
         current_file_path = os.path.abspath(__file__)
@@ -70,7 +84,6 @@ class DirectionPriceAPIView(APIView):
         for plane in range(len(airline)):
             if flagOfClas and (plane == 3 or plane == 6):
                 NormPlane = (plane - Mean[7]) / Std[7]
-                
                 test = np.array([[i for i in response + [NormPlane]]])
                 y_pred = model.predict(test)
                 y_pred = [i for i in y_pred]
@@ -79,7 +92,6 @@ class DirectionPriceAPIView(APIView):
                     result += [[airline[plane], y_pred[0][0] * StdNorm + MeanNorm]]
             elif not(flagOfClas):
                 NormPlane = (plane - Mean[7]) / Std[7]
-                
                 test = np.array([[i for i in response + [NormPlane]]])
                 y_pred = model.predict(test)
                 y_pred = [i for i in y_pred]
