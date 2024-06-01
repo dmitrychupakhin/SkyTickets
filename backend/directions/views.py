692350d918a00f3786b567f9da9e9073b03f6530
vituyac <skisterev78@mail.ru>
2024-06-01 16:15:25 +0300

    add model

--- backend/directions/views.py
+++ backend/directions/views.py
@@ -3,8 +3,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
-#from tensorflow import keras
-#import numpy as np
+from tensorflow import keras
+import numpy as np
+from rest_framework import status
+import os
 
 class AllDirectionsAPIView(generics.ListAPIView):
     serializer_class = AllDirSerializer
@@ -25,9 +27,14 @@ class AllDetailDirectionsAPIView(generics.ListAPIView):
 class DirectionPriceAPIView(APIView):
     
     def get(self, request):
-        model_loaded = keras.models.load_model("../../model/modelForEconomy")
-        test = np.array([[-1.28, 1.21, 1.14, -0.98, 0.31, 0.17, 0.19]])
+        current_file_path = os.path.abspath(__file__)
+        current_file_path = current_file_path[:-(len("views.py"))][:-len("directions/")] + "model/Avia.h5"
+        #model_loaded = keras.models.load_model("../../model/Avia.h5")
+        print(current_file_path)
+        model_loaded = keras.models.load_model(current_file_path)
+        test = np.array([[-1.28, 1.21, 1.14, -0.98, 0.31, 0.17, 0.19, 1]])
         y_pred = model_loaded.predict(test)
-        StdNorm = 3.74
-        MeanNorm = 6.57
-        print(y_pred * StdNorm + MeanNorm)
+        StdNorm = 22683.86
+        MeanNorm = 20875.77
+        result = y_pred * StdNorm + MeanNorm
+        return Response({'result': result}, status=status.HTTP_200_OK)
