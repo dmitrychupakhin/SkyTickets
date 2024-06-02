61cfd11e2392cc03b5dfc0d63adaf1a55e45b425
vituyac <skisterev78@mail.ru>
2024-06-02 10:40:55 +0300

    model

--- backend/directions/views.py
+++ backend/directions/views.py
@@ -69,21 +69,19 @@ class DirectionByIdAPIView(generics.RetrieveAPIView):
 class DirectionPriceAPIView(APIView):
     
     def post(self, request):
-        StdNorm = 22683.868561457934
-        MeanNorm = 20875.7709068807
         
         directionfrom = Direction.objects.get(id=request.data['from'])
         directionto = Direction.objects.get(id=request.data['to'])
         fr = directionfrom.city
         to = directionto.city
         fr = fr.replace('Мумбаи', 'Mumbai').replace('Дели','Delhi').replace('Калькутта', 'Kolkata').replace('Хайдарабад', 'Hyderabad').replace('Ченнаи', 'Chennai').replace('Бангалор', 'Bangalore')
-        to = fr.replace('Мумбаи', 'Mumbai').replace('Дели','Delhi').replace('Калькутта', 'Kolkata').replace('Хайдарабад', 'Hyderabad').replace('Ченнаи', 'Chennai').replace('Бангалор', 'Bangalore')
+        to = to.replace('Мумбаи', 'Mumbai').replace('Дели','Delhi').replace('Калькутта', 'Kolkata').replace('Хайдарабад', 'Hyderabad').replace('Ченнаи', 'Chennai').replace('Бангалор', 'Bangalore')
         # date / dep_time / from / to / clas / time_taken / stop -> str
         #response = ["12-03", "09:00", "Mumbai", "Delhi", "economy", "10h 50m", "0"]
         response = [request.data['date'], request.data['dep_time'], fr, to, request.data['clas'], request.data['time_taken'], request.data['stop']]
-    
-        From = ['Mumbai', 'Delhi', 'Hyderabad', 'Chennai', 'Bangalore', 'Kolkata']
-        To = ['Mumbai', 'Delhi', 'Hyderabad', 'Chennai', 'Bangalore', 'Kolkata']
+        print(response)
+        From = ['Hyderabad', 'Kolkata', 'Mumbai', 'Delhi', 'Chennai', 'Bangalore']
+        To = ['Hyderabad', 'Kolkata', 'Mumbai', 'Delhi', 'Chennai', 'Bangalore']
         Mean = [283.919857, 781.114409, 2.330489, 2.385270, 0.311351, 732.907093, 0.923924, 3.942571]
         Std = [45.747274, 321.879003, 1.767264, 1.769400, 0.463047, 431.536740, 0.397781, 1.929122]
         response[0] = int(response[0][3:5] + response[0][:2])
@@ -96,27 +94,32 @@ class DirectionPriceAPIView(APIView):
         flagOfClas = 1 if response[4] else 0
         for i in range(7):
             response[i] = (response[i] - Mean[i]) / Std[i]
-        airline = ['SpiceJet', 'GO FIRST', 'Indigo', 'Air India', 'AirAsia', 'Trujet', 'Vistara', 'StarAir']
+        airline = ['GO FIRST', 'StarAir', 'Indigo', 'Vistara', 'AirAsia', 'Trujet', 'Air India', 'SpiceJet']
         result = []
         current_file_path = os.path.abspath(__file__)
-        current_file_path = current_file_path[:-(len("views.py"))][:-len("directions/")] + "model/Avia.h5"
+        current_file_path = current_file_path[:-(len("views.py"))][:-len("directions/")] + "model/Avia2.h5"
         model = keras.models.load_model(current_file_path)
         for plane in range(len(airline)):
             if flagOfClas and (plane == 3 or plane == 6):
                 NormPlane = (plane - Mean[7]) / Std[7]
-                test = np.array([[i for i in response + [NormPlane]]])
+                rightData = [i for i in response + [NormPlane]]
+                rightData = [rightData[0], rightData[7], rightData[1], rightData[2], rightData[5], rightData[6], rightData[3], rightData[4]]
+                test = np.array([rightData])
                 y_pred = model.predict(test)
                 y_pred = [i for i in y_pred]
-                print(y_pred[0][0]* StdNorm + MeanNorm)
-                if y_pred[0][0]* StdNorm + MeanNorm > 20_000:
-                    result += [[airline[plane], y_pred[0][0] * StdNorm + MeanNorm]]
+                print(y_pred[0][0])
+                if y_pred[0][0] > 20_000:
+                    result += [[airline[plane], y_pred[0][0]]]
             elif not(flagOfClas):
                 NormPlane = (plane - Mean[7]) / Std[7]
-                test = np.array([[i for i in response + [NormPlane]]])
+                rightData = [i for i in response + [NormPlane]]
+                rightData = [rightData[0], rightData[7], rightData[1], rightData[2], rightData[5], rightData[6], rightData[3], rightData[4]]
+                test = np.array([rightData])
                 y_pred = model.predict(test)
                 y_pred = [i for i in y_pred]
-                if 1200 < y_pred[0][0]* StdNorm + MeanNorm < 25_000:
-                    result += [airline[plane] + ": " + str(y_pred[0][0] * StdNorm + MeanNorm)]
+                if 1200 < y_pred[0][0] < 25_000:
+                    result += [airline[plane] + ": " + str(y_pred[0][0])]
+
         return Response({'result': result}, status=status.HTTP_200_OK)
     
 class FactsAPIView(APIView):
