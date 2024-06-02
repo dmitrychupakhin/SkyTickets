ba3e6637ce0383abfdf5ad4f031ed24d5a6b7d72
vituyac <skisterev78@mail.ru>
2024-06-02 03:40:44 +0300

    pagination v1

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -128,7 +128,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 4,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
